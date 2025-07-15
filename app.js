570f7bae26f3884280603bd9d95b2d25cf7daa8f
khanhluan12 <155692902+khanhluan12@users.noreply.github.com>
2025-07-15 14:00:11 +0700

    sa

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/registrations', registrationRoutes);
 const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
+     console.log('✅ Connected to MongoDB Atlas!');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error('MongoDB connection error:', err));
